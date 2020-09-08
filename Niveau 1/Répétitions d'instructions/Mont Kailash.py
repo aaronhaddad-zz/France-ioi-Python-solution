e79ac2ac2185796c824ad2c7e660efe206536e10
Aaron Haddad <48856245+aaronhaddad@users.noreply.github.com>
2020-09-08 20:46:12 +0200

    Create Mont Kailash.py

---
+++ Niveau 1/Répétitions d'instructions/Mont Kailash.py
@@ -0,0 +1,10 @@
+from robot import *
+for loop in range(108):
+    for loop in range(13):
+        haut()
+    for loop in range(13):
+        droite()
+    for loop in range(13):
+        bas()
+    for loop in range(13):
+        gauche()
