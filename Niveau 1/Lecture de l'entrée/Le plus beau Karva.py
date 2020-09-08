42edca2fc612beaf8a39e8860814253519f59bd2
Aaron Haddad <48856245+aaronhaddad@users.noreply.github.com>
2020-09-08 20:57:02 +0200

    Create Le plus beau Karva.py

---
+++ Niveau 1/Lecture de l'entrée/Le plus beau Karva.py
@@ -0,0 +1,7 @@
+nbKarvas=int(input())
+for i in range(nbKarvas):
+    poids=int(input())
+    age=int(input())
+    longueur=int(input())
+    hauteur=int(input())
+    print((longueur*hauteur)+poids)
